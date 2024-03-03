c26948131ec18206218de92e7a42273008f3b973
xingege <shuizhengqi1@163.com>
2024-03-03 20:49:44 +0800

    1.显示一下剩余的时间

--- file/file_md5.py
+++ file/file_md5.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import hashlib
+import os
 import sys
 import time
 from tqdm import tqdm
@@ -40,9 +41,14 @@ def cal_all_md5():
 def calculate_md5(file_id, file_path):
     start_time = time.time()
     hash_md5 = hashlib.md5()
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    file_size = os.stat(file_path).st_size
+    with open(file_path, "rb") as f, tqdm(total=file_size,
+                                          bar_format="文件读取进度：{percentage:3.0f}%|{bar}|已处理{n_fmt}/总数{total_fmt}",
+                                          smoothing=0) as file_bar:
+        batch_size = 8192
+        for chunk in iter(lambda: f.read(batch_size), b""):
             hash_md5.update(chunk)
+            file_bar.update(batch_size)
     end_time = time.time()  # 结束计时
     elapsed_time = end_time - start_time  # 计算耗时
     file_info_db.update_file_md5(file_id, hash_md5.hexdigest())
