daa25f7f4e8fd1fdccd3b5d189f8c3eb4d5b7312
xingege <shuizhengqi1@163.com>
2024-03-03 23:49:01 +0800

    1.文件扫描的时候，过滤掉已经存在的数据

--- web/pic.py
+++ web/pic.py
@@ -1,11 +1,12 @@
 from typing import Union
 from fastapi import APIRouter
 from picture import face_detect
+from tool import executor_tool
 
 app = APIRouter()
 
 
 @app.get("/face_detect")
 async def run_face_detect():
-    face_detect.process_all_pic()
+    executor_tool.file_pool.submit(face_detect.process_all_pic)
     return "成功检测人脸"
