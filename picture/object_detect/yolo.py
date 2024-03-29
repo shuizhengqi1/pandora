2d5f099bcc1d95208557adcb0fc86d652b55b140
yanghengxing <yanghengxing@meituan.com>
2024-03-29 18:32:40 +0800

    1

--- picture/object_detect/yolo.py
+++ picture/object_detect/yolo.py
@@ -1,23 +1,26 @@
 from ultralytics import YOLO
 from PIL import Image
+import torch
+from torchvision.transforms import functional as F
+
 import cv2
+
+local_model_path = "/Users/yanghengxing/PycharmProjects/pandora/model/yolov8x.pt"
 
 
 def detect(path):
-    model = YOLO("../../data/yolov8x-oiv7.pt")
-    model.info()
-    image = Image.open(path)
-    # from PIL
-    result = model.predict(image, stream=False, conf=0.5)[0]  # sazve plotted images
-    detected_objects = []
-    for box in result.boxes:
-        detected_objects.append(result.names.get(box[-1].item()))
-    print(detected_objects)
-    for cls in result.boxes.cls:
-        print(f"检测到的名在:{result.names[int(cls.item())]}")
-
-    r.save_crop("./runs")
-    print(f"检测到的结果是:{detected_objects}")
+    model = YOLO(local_model_path)
+    # 读取图片
+    img = Image.open(path)
+    # 图片预处理
+    # 物体检测
+    results = model.predict(source=img, save=True, save_txt=True, conf=0.5)
+    # 解析结果
+    for i, r in enumerate(results):
+        im_bgr = r.plot()  # BGR-order numpy array
+        im_rgb = Image.fromarray(im_bgr[..., ::-1])
+        #保存到指定的地点
+        r.save(filename=f'result-{i}.jpg')
 
 
-detect("/Users/yanghengxing/Pictures/WechatIMG205.jpg")
+detect("/Users/yanghengxing/Pictures/wallpaa.jpeg")
