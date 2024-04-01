3dfab879a9c571fdd8fde521ea20b323e84a2302
yanghengxing <yanghengxing@meituan.com>
2024-04-01 14:55:02 +0800

    1.完成配置的保存，以及列表上配置的展示

--- web/frontend/config_page.py
+++ web/frontend/config_page.py
@@ -5,21 +5,48 @@ from web.api import config_api
 
 router = APIRouter()
 
+config_map = None
+
+
+@ui.refreshable
+async def config_table() -> None:
+    with ui.column():
+        global config_map
+        config_map = await config_api.show_config_info()
+        # 扫描地址
+        with ui.row():
+            ui.label("扫描地址").style("width: 200px;display: flex;align-items: center;vertical-align: middle")
+            ui.input(value=config_map["start_dir"]).bind_value_to(config_map, "start_dir",
+                                                                  lambda value: value).style("width: 300px")
+            # 扫描地址
+        with ui.row():
+            ui.label("忽略的目录").style("width: 200px;display: flex;align-items: center;vertical-align: middle")
+            ui.input(value=config_map['skip_dir_name']).bind_value_to(config_map, "skip_dir_name",
+                                                                      lambda value: value).style("width: 300px")
+        with ui.row():
+            ui.label("进度保存目录").style("width: 200px;display: flex;align-items: center;vertical-align: middle")
+            ui.input(value=config_map['scan_process_path']).bind_value_to(config_map, "scan_process_path",
+                                                                          lambda value: value).style("width: 300px")
+
 
 @router.page("/", title="配置读取")
-async def config_table():
+async def config_page_index():
     with page_template.frame("配置查看"):
-        with ui.column():
-            config_list = await config_api.show_config_info()
-            for config in config_list:
-                with ui.row():
-                    # 使用 ui.label 创建标签
-                    label = ui.label(config['fieldKey']).style("width: 200px;display: "
-                                                               "flex;align-items: center;vertical-align: middle")
-                    # 设置标签的宽度，避免过长
-                    # 使用 ui.input 创建输入框
-                    input_box = ui.input(value=config['fieldValue']).style("width: 300px")
-                    # 设置输入框的宽度，使其不超过屏幕宽度
-            with ui.row():
-                submit_button = ui.button("保存")
-                refresh_button = ui.button("刷新")
+        with ui.row():
+            await config_table()
+        with ui.row():
+            ui.button("保存", on_click=lambda: save_config(config_map))
+            ui.button("刷新", on_click=lambda: config_table.refresh())
+
+
+async def save_config(config_map):
+    print(f"new value is {config_map}")
+    await config_api.change_config(config_map)
+    ui.notify(f"配置更新成功")
+
+
+async def refresh_config_info():
+    # 重新获取配置信息并更新config_map
+    new_config_map = await config_api.show_config_info()
+    # 更新字典，这里需要注意线程安全和数据同步的问题
+    config_map.update(new_config_map)
